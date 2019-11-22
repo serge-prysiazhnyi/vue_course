4344f83b77093c944aa7d1e132d4e135931d0246
Prysiazhnyi Serge <prysiazhnyi.s@brights.cloud>
2019-11-22 17:04:24 +0200

    car component added, app updated

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    
+    <app-car 
+      :name="name" 
+      :desc='desc' 
+      :year="year"
+    ></app-car>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Car from './components/car.vue'
 
 export default {
+  data() {
+    return {
+      name: 'test',
+      desc: 'test',
+      year: 1970
+    }
+  },
   name: 'app',
   components: {
-    // HelloWorld
-  }
+    appCar: Car
+  },
+  methods: {
+    
+  },
 }
 </script>
 
---
+++ src/components/car.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <p class="year">{{ year }}</p>
+        <p>{{ desc }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            default: ''
+        },
+        desc: {
+            type: String,
+            default: ''
+        },
+        year: {
+            type: Number
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        text-transform: uppercase;
+        p {
+            font-size: 16px;
+        }
+        .year {
+            font-size: 20px;
+        }
+    }
+</style>
