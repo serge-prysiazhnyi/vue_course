6332cfc96e6c7c0b7a44620bddffd54e78707642
Prysiazhnyi Serge <prysiazhnyi.s@brights.cloud>
2019-12-19 13:16:46 +0200

    remove unused components

--- src/color.js
+++
@@ -1,9 +0,0 @@
-export default {
-/* eslint-disable no-console */
-    bind(el, bindings, vnode) {
-        el.style.color = "red";
-        console.log(bindings);
-        console.log(vnode);
-    }
-/* eslint-enable no-console */
-}
--- src/main.js
+++ src/main.js
@@ -1,30 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueResource from 'vue-resource';
-// import colorDerective from './color';
 
 /* eslint-disable no-console */
-// todo
-// Vue.derective('colored');
 
-Vue.filter('uppercase', (value) => {
-  return value.toUpperCase();
-})
-
-Vue.use(VueResource);
-
-Vue.http.options.root = 'http://localhost:3000/';
-
-Vue.http.interceptors.push(request =>{
-  
-  request.headers.set('Auth', 'token ' + Math.random())
-})
-
-// Vue.mixin({
-//   beforeCreate() {
-//     console.log('beforeCreate');
-//   },
-// })
 
 Vue.config.productionTip = false;
 
